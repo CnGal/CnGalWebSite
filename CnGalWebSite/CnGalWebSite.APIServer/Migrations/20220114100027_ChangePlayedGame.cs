98d60885174f80540cefc10a9dd26fe64d3e18ce
LittleFish-233 <LittleFishTenyears@outlook.com>
2022-01-14 18:10:43 +0800

    修复Staff没有设置主图时错误显示主图的Bug

---
+++ CnGalWebSite/CnGalWebSite.APIServer/Migrations/20220114100027_ChangePlayedGame.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CnGalWebSite.APIServer.Migrations
+{
+    public partial class ChangePlayedGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInSteam",
+                table: "PlayedGames",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<long>(
+                name: "PlayDuration",
+                table: "PlayedGames",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "PlayedGames",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                column: "ConcurrencyStamp",
+                value: "7cd1d3b4-e914-4c20-a1cd-950923934583");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e576",
+                column: "ConcurrencyStamp",
+                value: "77c6af35-ca5c-43c7-93cc-3de4f9fee336");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e577",
+                column: "ConcurrencyStamp",
+                value: "4de6d4b4-59c2-4b3f-820d-ee86201a8b57");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e578",
+                column: "ConcurrencyStamp",
+                value: "9e4ad5e2-8b7d-487e-975b-ec76b0d7c2c3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "RegistTime" },
+                values: new object[] { "3fdf21e8-bdcc-4ff6-a693-5f505800ded1", new DateTime(2022, 1, 14, 18, 0, 26, 702, DateTimeKind.Unspecified).AddTicks(2868) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInSteam",
+                table: "PlayedGames");
+
+            migrationBuilder.DropColumn(
+                name: "PlayDuration",
+                table: "PlayedGames");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "PlayedGames");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                column: "ConcurrencyStamp",
+                value: "411fb597-c5da-4051-9041-05b1507760bc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e576",
+                column: "ConcurrencyStamp",
+                value: "1d3e3fa7-3ce0-46de-ac93-7474f277a75f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e577",
+                column: "ConcurrencyStamp",
+                value: "192bf073-ed3f-4aff-a0d0-2a93b3cf584f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e578",
+                column: "ConcurrencyStamp",
+                value: "bc982083-fcce-4366-a9c6-5de8271460b1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "RegistTime" },
+                values: new object[] { "f9804148-ff46-4bff-88a4-5d454ce5e06e", new DateTime(2022, 1, 8, 17, 37, 21, 67, DateTimeKind.Unspecified).AddTicks(290) });
+        }
+    }
+}
