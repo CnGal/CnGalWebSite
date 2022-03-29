e7bfa62b9d60c7d56418597e215535445e2385d1
LittleFish-233 <LittleFishTenyears@outlook.com>
2022-03-29 20:14:25 +0800

    添加SteamId1933640的临时数据

--- CnGalWebSite/CnGalWebSite.APIServer/Application/SteamInfors/SteamInforService.cs
+++ CnGalWebSite/CnGalWebSite.APIServer/Application/SteamInfors/SteamInforService.cs
@@ -235,7 +235,7 @@ namespace CnGalWebSite.APIServer.Application.SteamInfors
                 steam.CutNow = 0;
                 steam.PriceNowString = "¥ " + ((double)steam.PriceNow / 100).ToString("0.00");
             }
-            if (steamId == 1827680 || steamId == 1903370 || steamId == 1874810)
+            if (steamId == 1827680 || steamId == 1903370 || steamId == 1874810 || steamId == 1933640)
             {
                 steam.PriceNow = 0;
                 steam.CutNow = 0;
