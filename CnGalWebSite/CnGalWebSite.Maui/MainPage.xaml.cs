82d6a6b29328100e56e5b5eeb6201d055a73f45d
LittleFish-233 <LittleFishTenyears@outlook.com>
2022-04-01 17:30:36 +0800

    调用错误提示之前先判空

--- CnGalWebSite/CnGalWebSite.Maui/MainPage.xaml.cs
+++ CnGalWebSite/CnGalWebSite.Maui/MainPage.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace CnGalWebSite.Maui
+﻿using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace CnGalWebSite.Maui
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
+
             InitializeComponent();
+
+            blazorWebView.ExternalNavigationStarting +=
+                (sender, externalLinkNavigationEventArgs) =>
+                {
+                    externalLinkNavigationEventArgs.ExternalLinkNavigationPolicy = ExternalLinkNavigationPolicy.InsecureOpenInWebView;
+                };
+
         }
     }
 }
