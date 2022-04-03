35a1b3c59cf6102c3be2cadc01db2820bff83639
LittleFish-233 <LittleFishTenyears@outlook.com>
2022-04-03 23:38:41 +0800

    投稿工具适配小黑盒平台

--- CnGalWebSite/CnGalWebSite.DataModel/ViewModel/HistoryData/ZhiHuArticleModel.cs
+++ CnGalWebSite/CnGalWebSite.DataModel/ViewModel/HistoryData/ZhiHuArticleModel.cs
@@ -1,24 +1,5 @@
 ﻿using System;
 namespace CnGalWebSite.DataModel.ViewModel.HistoryData
 {
-    public class ZhiHuArticleModel
-    {
-        public long Id { get; set; }
 
-        public string Title { get; set; }
-
-        public string OriginalAuthor { get; set; }
-
-        public string Url { get; set; }
-
-        public string MainPage { get; set; }
-
-        public string Image { get; set; }
-
-        public bool IsCutImage { get; set; }
-
-        public DateTime PublishTime { get; set; }
-
-        public DateTime? PostTime { get; set; }
-    }
 }
