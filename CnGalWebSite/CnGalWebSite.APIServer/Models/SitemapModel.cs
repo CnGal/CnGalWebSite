5cc31fbc2f110d27851478b46d297c09f4e47123
LittleFish-233 <LittleFishTenyears@outlook.com>
2023-03-25 12:37:58 +0800

    添加sitemap.xml

---
+++ CnGalWebSite/CnGalWebSite.APIServer/Models/SitemapModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CnGalWebSite.APIServer.Models
+{
+    public class SitemapModel
+    {
+        public string Loc { get; set; }
+        public string Priority { get; set; }
+        public string ChangeFreq { get; set; }
+        public string LastModified { get; set; }
+    } 
+
+}
