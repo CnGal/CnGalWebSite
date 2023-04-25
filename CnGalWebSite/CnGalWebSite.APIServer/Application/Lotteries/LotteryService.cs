cebf3ffb9b2971b4050c9cadcaa88f2dfe64ea1c
LittleFish-233 <LittleFishTenyears@outlook.com>
2023-04-25 14:03:46 +0800

    修复同步预约抽奖用户时号码分配重复的bug

--- CnGalWebSite/CnGalWebSite.APIServer/Application/Lotteries/LotteryService.cs
+++ CnGalWebSite/CnGalWebSite.APIServer/Application/Lotteries/LotteryService.cs
@@ -391,6 +391,7 @@ namespace CnGalWebSite.APIServer.Application.Lotteries
             var users = booking.Users.Where(s => lottery.Users.Any(x => x.ApplicationUserId == s.ApplicationUserId) == false).Select(s => s.ApplicationUser);
 
             var time = DateTime.Now.ToCstTime();
+            var count = lottery.Users.Count;
             foreach (var item in users)
             {
                 await _lotteryUserRepository.InsertAsync(new LotteryUser
@@ -398,7 +399,7 @@ namespace CnGalWebSite.APIServer.Application.Lotteries
                     ApplicationUserId = item.Id,
                     LotteryId = lottery.Id,
                     ParticipationTime = time,
-                    Number = lottery.Users.Count + 1,
+                    Number = count + 1,
                     //查找是否有相同的特征值
                     IsHidden = await _operationRecordService.CheckOperationRecord(OperationRecordType.Lottery, lottery.Id.ToString(), item)
                 });
