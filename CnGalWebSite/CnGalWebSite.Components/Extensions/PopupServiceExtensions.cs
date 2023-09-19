33365d4a33f604832ea2d5ffb996f5427fee90f9
LittleFish-233 <LittleFishTenyears@outlook.com>
2023-09-19 09:04:08 +0800

    迁移完毕组件部分

---
+++ CnGalWebSite/CnGalWebSite.Components/Extensions/PopupServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using BlazorComponent;
+using Masa.Blazor;
+using Masa.Blazor.Presets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnGalWebSite.Components.Extensions
+{
+    public static class PopupServiceExtensions
+    {
+        public static async Task ToastErrorAsync(this IPopupService popupService, string title, string message)
+        {
+            await popupService.ToastAsync(title, message, AlertTypes.Error);
+        }
+        public static async Task ToastWarningAsync(this IPopupService popupService, string title, string message)
+        {
+            await popupService.ToastAsync(title, message, AlertTypes.Warning);
+        }
+        public static async Task ToastInfoAsync(this IPopupService popupService, string title, string message)
+        {
+            await popupService.ToastAsync(title, message, AlertTypes.Info);
+        }
+        public static async Task ToastSuccessAsync(this IPopupService popupService, string title, string message)
+        {
+            await popupService.ToastAsync(title, message, AlertTypes.Success);
+        }
+
+        public static async Task ToastAsync(this IPopupService popupService, string title, string message, AlertTypes type)
+        {
+            await popupService.ToastAsync(new ToastConfig
+            {
+                Type = type,
+                Title = title,
+                Content = message,
+            });
+        }
+
+    }
+}
