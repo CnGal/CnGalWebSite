291a1c6a5f40a4693381eede7d857e086ae12a1e
沙雕の方块 <littlefishtenyears@outlook.com>
2023-10-06 10:34:06 +0800

    添加用户认证

---
+++ CnGalWebSite/CnGalWebSite.ProjectSite.Shared/Models/Users/EntryInforTipViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnGalWebSite.ProjectSite.Shared.Models.Users
+{
+    public class EntryInforTipViewModel
+    {
+        public int Id { get; set; }
+        public EntryType Type { get; set; }
+        public string Name { get; set; }
+        public string MainImage { get; set; }
+        public string BriefIntroduction { get; set; }
+    }
+
+    public enum EntryType
+    {
+        Game,
+        Role,
+        ProductionGroup,
+        Staff
+    }
+
+}
