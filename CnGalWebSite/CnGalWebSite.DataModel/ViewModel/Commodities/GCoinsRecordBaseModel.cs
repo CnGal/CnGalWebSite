b33af068e0897bd6c0b5f326ff91850396e64c4f
沙雕の方块 <littlefishtenyears@outlook.com>
2024-09-17 10:20:40 +0800

    支持查看G币记录，添加用户权重的抽奖

---
+++ CnGalWebSite/CnGalWebSite.DataModel/ViewModel/Commodities/GCoinsRecordBaseModel.cs
@@ -0,0 +1,25 @@
+﻿using CnGalWebSite.DataModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnGalWebSite.DataModel.ViewModel.Commodities
+{
+    public class GCoinsRecordBaseModel
+    {
+        public DateTime Time { get; set; }
+
+        public int Count { get; set; }
+
+        public string Note { get; set; }
+
+        public UserIntegralSourceType SourceType { get; set; }
+    }
+
+    public class GCoinsRecordOverviewModel : GCoinsRecordBaseModel
+    {
+
+    }
+}
