6009ab78984375a13a278d09f3dd3a41d55b965e
沙雕の方块 <littlefishtenyears@outlook.com>
2025-07-02 23:27:45 +0800

    添加更多抽奖任务

--- CnGalWebSite/CnGalWebSite.DataModel/ViewModel/Expo/ExpoUserTaskModel.cs
+++ CnGalWebSite/CnGalWebSite.DataModel/ViewModel/Expo/ExpoUserTaskModel.cs
@@ -24,13 +24,53 @@ namespace CnGalWebSite.DataModel.ViewModel.Expo
         public bool IsPickUpSharedGames { get; set; }
 
         /// <summary>
-        /// 是否已签到
+        /// 今日是否已签到
         /// </summary>
         public bool IsSignIn { get; set; }
 
         /// <summary>
-        /// 抽奖次数
+        /// 签到总天数
         /// </summary>
-        public int LotteryCount { get; set; }
+        public int SignInDays { get; set; }
+
+        /// <summary>
+        /// 是否填写问卷
+        /// </summary>
+        public bool IsSurvey { get; set; }
+
+        /// <summary>
+        /// 是否给游戏评分
+        /// </summary>
+        public bool IsRateGame { get; set; }
+
+        /// <summary>
+        /// 是否绑定群聊QQ
+        /// </summary>
+        public bool IsBindQQ { get; set; }
+
+        /// <summary>
+        /// 是否更换默认头像
+        /// </summary>
+        public bool IsChangeAvatar { get; set; }
+
+        /// <summary>
+        /// 是否更换默认签名
+        /// </summary>
+        public bool IsChangeSignature { get; set; }
+
+        /// <summary>
+        /// 是否填写国G世代
+        /// </summary>
+        public bool IsSaveGGeneration { get; set; }
+
+        /// <summary>
+        /// 总点数
+        /// </summary>
+        public int TotalPoints { get; set; }
+
+        /// <summary>
+        /// 抽奖次数（向下兼容，等于 TotalPoints / 100）
+        /// </summary>
+        public int LotteryCount => TotalPoints / 100;
     }
 }
