2dac5abdfdf82f5f0331f402b7c80d2083fabdc5
沙雕の方块 <littlefishtenyears@outlook.com>
2026-03-18 21:54:50 +0800

    优化编辑样式

--- CnGalWebSite/CnGalWebSite.SDK.MainSite/Models/TagEdit/TagEditMetaOptions.cs
+++ CnGalWebSite/CnGalWebSite.SDK.MainSite/Models/TagEdit/TagEditMetaOptions.cs
@@ -1,3 +1,5 @@
+using CnGalWebSite.DataModel.ViewModel.Tags;
+
 namespace CnGalWebSite.SDK.MainSite.Models.TagEdit;
 
 public sealed class TagEditMetaOptions
@@ -5,4 +7,6 @@ public sealed class TagEditMetaOptions
     public List<string> TagItems { get; set; } = [];
 
     public List<string> EntryItems { get; set; } = [];
+
+    public List<TagTreeModel> TagTreeItems { get; set; } = [];
 }
