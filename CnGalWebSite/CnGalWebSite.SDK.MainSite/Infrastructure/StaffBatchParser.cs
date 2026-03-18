91d456d968afa6c63b51327ff51a808121ce58e3
沙雕の方块 <littlefishtenyears@outlook.com>
2026-03-18 23:55:08 +0800

    优化音频卡片样式，优化创建按钮样式

--- CnGalWebSite/CnGalWebSite.SDK.MainSite/Infrastructure/StaffBatchParser.cs
+++ CnGalWebSite/CnGalWebSite.SDK.MainSite/Infrastructure/StaffBatchParser.cs
@@ -65,7 +65,7 @@ public static class StaffBatchParser
                     continue;
                 }
                 var roleName = MidStrEx(infor, "(", ")");
-                var type = GetGeneralType(positionGeneral ?? position);
+                var type = StaffHelper.GetGeneralType(positionGeneral ?? position);
                 result.Add(new StaffModel
                 {
                     Name = infor.Replace($"({roleName})", "").Trim(),
@@ -78,37 +78,6 @@ public static class StaffBatchParser
         }
 
         return result;
-    }
-
-    private static PositionGeneralType GetGeneralType(string text)
-    {
-        foreach (var item in Enum.GetValues(typeof(PositionGeneralType)))
-        {
-            var temp = (PositionGeneralType)item;
-            if (text == temp.GetDisplayName())
-            {
-                return temp;
-            }
-        }
-
-        if (text.Contains("原画") || text.Contains("画师"))
-        {
-            return PositionGeneralType.FineArts;
-        }
-        else if (text.Contains("设计"))
-        {
-            return PositionGeneralType.FineArts;
-        }
-        else if ((text.Contains("配音") || text.Contains("声优") || text.ToUpper().Contains("CV") || text.ToUpper().Contains("CAST")) && !text.Contains("导演") && !text.Contains("监督") && !text.Contains("制作") && !text.Contains("后期") && !text.Contains("处理") && !text.Contains("后制"))
-        {
-            return PositionGeneralType.CV;
-        }
-        else if (text.Contains("感谢") || text.ToUpper().Contains("鸣谢") || text.ToUpper().Contains("致谢"))
-        {
-            return PositionGeneralType.SpecialThanks;
-        }
-
-        return PositionGeneralType.Other;
     }
 
     private static string MidStrEx(string source, string startStr, string endStr)
