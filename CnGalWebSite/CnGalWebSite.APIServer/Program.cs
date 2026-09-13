20836c7d8a5c1a8e9a68279fcfab7b490c5b9f68
Sliots <138327058+sliots@users.noreply.github.com>
2026-09-13 16:26:04 +0800

    revert(api): 恢复原有用户机密与命令行配置加载

--- CnGalWebSite/CnGalWebSite.APIServer/Program.cs
+++ CnGalWebSite/CnGalWebSite.APIServer/Program.cs
@@ -16,6 +16,14 @@ namespace CnGalWebSite.APIServer
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddCommandLine(args);//设置添加命令行
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddUserSecrets<Startup>();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
